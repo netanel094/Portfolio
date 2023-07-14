e480e18f7c49a3faaabf2c7c860f0fbad6c4edeb
netanel094 <netanel094@gmail.com>
2023-07-14 17:51:14 +0300

    set the backgroung  image in about section and adding photos

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,10 @@
-// About.jsx
 import React from "react";
 import "./About.css";
 
 export default function About() {
-  return <div className="about-container">About Component</div>;
+  return (
+    <section id="about">
+      <div id="container-fluid"> </div>
+    </section>
+  );
 }
